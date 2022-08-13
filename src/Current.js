717d4300f8db9ce93bde0fc577d8a800b5354d2f
Olesia <102444640+logachov@users.noreply.github.com>
2022-08-13 03:58:56 +0300

    React app Weather

--- src/Current.js
+++ src/Current.js
@@ -1,36 +1,53 @@
+import React, { useState } from "react";
 import "./Current.css";
+import Currentdate from "./Currentdate"; 
 
-export default function Current() {
+export default function Current(props) {
+const [celsius, setCelsius] = useState("col-4 currentTemp");
+const [farenheit, setFarenheit] = useState("col-4 hiddenTemp");
+function convertUnit(event) {
+  event.preventDefault();
+  if (celsius === "col-4 currentTemp") {
+    setCelsius("col-4 hiddenTemp");
+    setFarenheit("col-4 currentTemp");
+  }
+  else {
+    setCelsius("col-4 currentTemp");
+    setFarenheit("col-4 hiddenTemp");
+  }
+}
+let farenheitTemp = Math.round(props.data.temperature * 9/5 + 32);
   return (
     <div className="current-form">
       <hr />
       <div className="row currentInfo">
         <div className="col-6">
-          <p className="currentCity">Amsterdam</p>
-          <p className="currentTime">Monday 16:50</p>
-          <p className="currentDescription">Clear</p>
+          <p className="currentCity">{props.data.city}</p>
+          <div className="currentTime"><Currentdate date={props.data.date}/></div>
+          <p className="currentDescription text-capitalize">{props.data.description}</p>
         </div>
         <div className="col-2">
           <img
             className="currentPicture"
-            src="http://openweathermap.org/img/w/04n.png"
-            alt="Clouds"
+            src={props.data.icon}
+            alt={props.data.description}
           />
         </div>
-        <div className="col-4 currentTemp">
-          <span className="TempC">25</span>
+        <div className={celsius}>
+          <span className="TempC"> {props.data.temperature} </span>
           <span className="TempC">°C</span>
-          <span className="secondTemp">→°F</span>
-          <p className="humidity">Humidity 35%</p>
-          <p className="wind">Wind 3 m/c</p>
+          <span className="secondTemp"><a className="secondTemp" href="/" onClick={convertUnit}>→°F</a></span>
+          <p className="humidityF">Humidity {props.data.humidity} %</p>
+          <p className="windF">Wind {props.data.wind} m/c</p>
         </div>
-        <div className="col-4 hiddenTemp">
-          <span className="TempF">25</span>
+        <div className={farenheit}>
+          <span className="TempF"> {farenheitTemp} </span>
           <span className="TempF">°F</span>
-          <span className="secondTemp">→°C</span>
-          <p className="humidityF">Humidity 35%</p>
-          <p className="windF">Wind 3 m/c</p>
+          <span className="secondTemp"><a className="secondTemp" href="/" onClick={convertUnit}>→°C</a></span>
+          <p className="humidityF">Humidity {props.data.humidity} %</p>
+          <p className="windF">Wind {props.data.wind} m/c</p>
         </div>
+        
       </div>
     </div>
   );
